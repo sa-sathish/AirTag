0fb08b3063f8c91091de5461d48bb8d7a426d826
Sathish_Work <sa.shanmugam@tawari.sa>
2021-08-11 11:23:12 +0300

    Initial Commit

---
+++ AirTagFinder/AirTagFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirTagFinderApp.swift
+//  AirTagFinder
+//
+//  Created by sathish s on 11/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AirTagFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirTagFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirTagFinder
+//
+//  Created by sathish s on 11/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
